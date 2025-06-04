0f209b69b29f26c7bb233293ab7b4404622d9b8b
Richard <rohde122195@gmail.com>
2025-06-03 23:51:28 -0400

    Added the functionality to search for books by ISBN, Author, and Title. Also added functionality to search for book reviews by ISBN.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,67 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    res.send(JSON.stringify(books,null,4));
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the ISBN parameter from the request URL and send the corresponding book's details
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Retrieve the author parameter from the request URL
+  const author = req.params.author;
+  //Create an array of all books created by the author
+  let booksByAuthor = [];
+  for(let isbn in books){
+    if(books[isbn].author == author) {
+        booksByAuthor.push(books[isbn]);
+    }
+  //Check to see if there are any objects in the array. If there are, send them via JSON.stringify method. If not, return an error.
+  }
+  if(booksByAuthor.length>0) {
+    return res.status(200).send(JSON.stringify(booksByAuthor,null,4));
+  }
+  else{
+    return res.status(404).send("No book found with author "+author);
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Retrieve the title parameter from the request URL
+  const title = req.params.title;
+  //Create an array of all books with that title
+  let booksByTitle = [];
+  for(let isbn in books){
+    if(books[isbn].title == title) {
+        booksByTitle.push(books[isbn]);
+    }
+  //Check to see if there are any objects in the array. If there are, send them via JSON.stringify method. If not, return an error.
+  }
+  if(booksByTitle.length>0) {
+    return res.status(200).send(JSON.stringify(booksByTitle,null,4));
+  }
+  else{
+    return res.status(404).send("No book found with title "+title);
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if(books[isbn]){
+    return res.status(200).send(JSON.stringify(books[isbn].reviews,null,4));
+  }
+  else{
+    return res.status(404).send("No book found with ISBN "+isbn);
+  }
 });
 
 module.exports.general = public_users;
